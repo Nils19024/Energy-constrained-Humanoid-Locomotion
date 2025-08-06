f6fb9ca7afd54b2db35e41cfb88d9bfcf8e3f3de
JohannesWeisbarth <weisbarthjohannes@gmail.com>
2025-08-06 10:54:48 +0200

    Update isaac_training.sh

--- IsaacLab/isaac_training.sh
+++ IsaacLab/isaac_training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=dllabdlc_gpu-rtx2080
-# #SBATCH --mem=50G
+#SBATCH --mem=50G
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus=1
-source IsaacEnv/bin/activate
+source ../IsaacEnv/bin/activate
 module load cuda/12.1
 srun ./isaaclab.sh -p scripts/reinforcement_learning/skrl/train.py --task Isaac-Velocity-Flat-G1-v0 --headless --num_envs 4096 --max_iterations 5000
