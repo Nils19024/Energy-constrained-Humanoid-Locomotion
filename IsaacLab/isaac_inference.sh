bcdc1b23648174ffdce8e82bf49f7d649d426d11
JohannesWeisbarth <weisbarthjohannes@gmail.com>
2025-08-06 10:29:04 +0200

    Update isaac_inference.sh

--- IsaacLab/isaac_inference.sh
+++ IsaacLab/isaac_inference.sh
@@ -3,6 +3,6 @@
 #SBATCH --mem=20G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=1
-source venv/bin/activate
+source IsaacEnv/bin/activate
 module load cuda/12.1
 srun ./isaaclab.sh -p scripts/reinforcement_learning/skrl/play.py --headless --video --video_length 400 --num_envs 1 --task Isaac-Velocity-Flat-G1-v0 --checkpoint /work/dlclarge2/weisbarj-dllab/IsaacLab/logs/skrl/g1_flat/2025-08-04_11-05-52_ppo_torch/checkpoints/best_agent.pt
