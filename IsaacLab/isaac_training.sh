62385676560a03b2e2724b16793ba70d133f6da8
JohannesWeisbarth <weisbarthjohannes@gmail.com>
2025-08-06 10:28:40 +0200

    Update isaac_training.sh

--- IsaacLab/isaac_training.sh
+++ IsaacLab/isaac_training.sh
@@ -3,6 +3,6 @@
 # #SBATCH --mem=50G
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus=1
-source venv/bin/activate
+source IsaacEnv/bin/activate
 module load cuda/12.1
 srun ./isaaclab.sh -p scripts/reinforcement_learning/skrl/train.py --task Isaac-Velocity-Flat-G1-v0 --headless --num_envs 4096 --max_iterations 5000
